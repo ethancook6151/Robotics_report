b8ed5d49dd1a5fd01dd485761a7b4566aaf7ba2a
Ethan <59934215+ethancook6151@users.noreply.github.com>
2022-02-28 11:23:37 -0500

    Changed the plan for the robot

--- simple_planner.py
+++ simple_planner.py
@@ -20,50 +20,87 @@ if __name__ == '__main__':
 	plan_point1 = Twist()
 	# just a quick solution to send two target points
 	# define a point close to the initial position
-	plan_point1.linear.x = -0.1
-	plan_point1.linear.y = -0.23
-	plan_point1.linear.z = 0.98
-	plan_point1.angular.x = math.pi/4
-	plan_point1.angular.y = math.pi/2
-	plan_point1.angular.z = -math.pi/4
+	plan_point1.linear.x = -0.5
+	plan_point1.linear.y = -0.15
+	plan_point1.linear.z = 0.314
+	plan_point1.angular.x = 2.99
+	plan_point1.angular.y = 0.18
+	plan_point1.angular.z = 1.5
 	# add this point to the plan
 	plan.points.append(plan_point1)
 	
 	plan_point2 = Twist()
 	# define a point away from the initial position
-	plan_point2.linear.x = -0.5
-	plan_point2.linear.y = -0.23
-	plan_point2.linear.z = 0.98
-	plan_point2.angular.x = math.pi/4
-	plan_point2.angular.y = math.pi/2
-	plan_point2.angular.z = -math.pi/4
+	plan_point2.linear.x = -0.7
+	plan_point2.linear.y = -0.15
+	plan_point2.linear.z = 0.0
+	plan_point2.angular.x = 2.99
+	plan_point2.angular.y = 0.18
+	plan_point2.angular.z = 1.5
 	# add this point to the plan
 	plan.points.append(plan_point2)
 	
 	plan_point3 = Twist()
 	# define a point away from the initial position
 	plan_point3.linear.x = -0.5
-	plan_point3.linear.y = -0.23
-	plan_point3.linear.z = 0.5
-	plan_point3.angular.x = math.pi/4
-	plan_point3.angular.y = math.pi/2
-	plan_point3.angular.z = -math.pi/4
+	plan_point3.linear.y = -0.15
+	plan_point3.linear.z = 0.314
+	plan_point3.angular.x = 2.99
+	plan_point3.angular.y = 0.18
+	plan_point3.angular.z = 1.5
 	# add this point to the plan
 	plan.points.append(plan_point3)
 
 	plan_point4 = Twist()
 	# define a point away from the initial position
-	plan_point4.linear.x = -0.1
-	plan_point4.linear.y = -0.23
-	plan_point4.linear.z = 0.5
-	plan_point4.angular.x = math.pi/4
-	plan_point4.angular.y = math.pi/2
-	plan_point4.angular.z = -math.pi/4
+	plan_point4.linear.x = -0.7
+	plan_point4.linear.y = -0.5
+	plan_point4.linear.z = 0.0
+	plan_point4.angular.x = 2.99
+	plan_point4.angular.y = 0.18
+	plan_point4.angular.z = 1.5
 	# add this point to the plan
 	plan.points.append(plan_point4)
+	
+	plan_point5 = Twist()
+	# just a quick solution to send two target points
+	# define a point close to the initial position
+	plan_point5.linear.x = -0.5
+	plan_point5.linear.y = -0.5
+	plan_point5.linear.z = 0.314
+	plan_point5.angular.x = 2.99
+	plan_point5.angular.y = 0.18
+	plan_point5.angular.z = 1.5
+	# add this point to the plan
+	plan.points.append(plan_point5)
+	
+	plan_point6 = Twist()
+	# just a quick solution to send two target points
+	# define a point close to the initial position
+	plan_point6.linear.x = -0.7
+	plan_point6.linear.y = 0.3
+	plan_point6.linear.z = 0.0
+	plan_point6.angular.x = 2.99
+	plan_point6.angular.y = 0.18
+	plan_point6.angular.z = 1.5
+	# add this point to the plan
+	plan.points.append(plan_point6)
+	
+	plan_point7 = Twist()
+	# just a quick solution to send two target points
+	# define a point close to the initial position
+	plan_point7.linear.x = -0.5
+	plan_point7.linear.y = 0.3
+	plan_point7.linear.z = 0.314
+	plan_point7.angular.x = 2.99
+	plan_point7.angular.y = 0.18
+	plan_point7.angular.z = 1.5
+	# add this point to the plan
+	plan.points.append(plan_point7)
 	
 	while not rospy.is_shutdown():
 		# publish the plan
 		plan_pub.publish(plan)
 		# wait for 0.1 seconds until the next loop and repeat
 		loop_rate.sleep()
+
